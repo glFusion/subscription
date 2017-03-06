d607e2ff9b68689d9ccc9b71b9c8cd4ca60a6949
Lee Garner <lee@leegarner.com>
2017-03-06 14:37:36 -0800

    Standardize upgrade routines

--- upgrade.inc.php
+++ upgrade.inc.php
@@ -3,9 +3,9 @@
 *   Upgrade routines for the Subscription plugin.
 *
 *   @author     Lee Garner <lee@leegarner.com>
-*   @copyright  Copyright (c) 2009-2016 Lee Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2009-2017 Lee Garner <lee@leegarner.com>
 *   @package    subscription
-*   @version    0.2.0
+*   @version    0.2.2
 *   @license    http://opensource.org/licenses/gpl-2.0.php 
 *               GNU Public License v2 or later
 *   @filesource
@@ -25,60 +25,71 @@ require_once SUBSCR_PI_PATH . "/sql/{$_DB_dbms}_install.php";
 *   Perform the upgrade starting at the current version.
 *
 *   @since  version 0.1.0
-*   @param  string  $current_ver    Current version to be upgraded
-*   @return integer                 Error code, 0 for success
+*   @return boolean     True on success, False on failure
 */
-function SUBSCR_do_upgrade($current_ver)
+function SUBSCR_do_upgrade()
 {
-    global $_TABLES, $_CONF, $_CONF_SUBSCR, $_SUBSCR_DEFAULTS;
+    global $_TABLES, $_CONF, $_CONF_SUBSCR, $_SUBSCR_DEFAULTS, $_PLUGIN_INFO;
 
-    $error = 0;
+    if (isset($_PLUGIN_INFO[$_CONF_SUBSCR['pi_name']])) {
+        $current_ver = $_PLUGIN_INFO[$_CONF_SUBSCR['pi_name']];
+    } else {
+        return false;
+    }
 
     $c = config::get_instance();
     $have_config = ($c->group_exists($_CONF_SUBSCR['pi_name'])) ? 1 : 0;
-    
-    if ($current_ver < '0.1.0') {
+
+    if (!COM_checkVersion($current_ver, '0.1.0')) {
+        $current_ver = '0.1.0';
         if ($have_config) {
             $c->add('displayblocks', $_SUBSCR_DEFAULTS['displayblocks'],
                 'select', 0, 0, 13, 210, true, $_CONF_SUBSCR['pi_name']);
         }
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    if ($current_ver < '0.1.1') {
-        $error = SUBSCR_do_upgrade_sql('0.1.1');
-        if ($error > 0) return $error;
+    if (!COM_checkVersion($current_ver, '0.1.1')) {
+        $current_ver = '0.1.1';
+        if (!SUBSCR_do_upgrade_sql($current_ver)) return false;
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    if ($current_ver < '0.1.2') {
-        $error = SUBSCR_do_upgrade_sql('0.1.2');
-        if ($error > 0) return $error;
+    if (!COM_checkVersion($current_ver, '0.1.2')) {
+        $current_ver = '0.1.2';
+        if (!SUBSCR_do_upgrade_sql($current_ver)) return false;
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    if ($current_ver < '0.1.3') {
-        $error = SUBSCR_do_upgrade_sql('0.1.3');
-        if ($error > 0) return $error;
+    if (!COM_checkVersion($current_ver, '0.1.3')) {
+        $current_ver = '0.1.3';
+        if (!SUBSCR_do_upgrade_sql($current_ver)) return false;
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    if ($current_ver < '0.1.4') {
+    if (!COM_checkVersion($current_ver, '0.1.4')) {
+        $current_ver = '0.1.4';
         if ($have_config) {
             $c->add('onmenu', $_SUBSCR_DEFAULTS['onmenu'],
                 'select', 0, 0, 3, 70, true, $_CONF_SUBSCR['pi_name']);
         }
-        $error = SUBSCR_do_upgrade_sql('0.1.3');
-        if ($error > 0) return $error;
+        if (!SUBSCR_do_upgrade_sql($current_ver)) return false;
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    if ($current_ver < '0.2.0') {
-        $error = SUBSCR_do_upgrade_sql('0.2.0');
-        if ($error > 0) return $error;
+    if (!COM_checkVersion($current_ver, '0.2.0')) {
+        $current_ver = '0.2.0';
+        if (!SUBSCR_do_upgrade_sql($current_ver)) return false;
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    if ($current_ver < '0.2.1') {
-        $error = SUBSCR_do_upgrade_sql('0.2.1');
-        if ($error > 0) return $error;
+    if (!COM_checkVersion($current_ver, '0.2.1')) {
+        $current_ver = '0.2.1';
+        if (!SUBSCR_do_upgrade_sql($current_ver)) return false;
+        if (!SUBSCR_do_set_version($current_ver)) return false;
     }
 
-    return $error;
+    return true;
 }
 
 
@@ -90,6 +101,7 @@ function SUBSCR_do_upgrade($current_ver)
 *   @since  version 0.1.0
 *   @param  string  $version    Version being upgraded TO
 *   @param  array   $sql        Array of SQL statement(s) to execute
+*   @return boolean         True on success, False on failure
 */
 function SUBSCR_do_upgrade_sql($version='')
 {
@@ -101,17 +113,46 @@ function SUBSCR_do_upgrade_sql($version='')
     }
 
     // Execute SQL now to perform the upgrade
-    COM_errorLOG("--Updating Subscription to version $version");
+    COM_errorLog("--Updating Subscription to version $version");
     foreach($SUBSCR_UPGRADE[$version] as $sql) {
-        COM_errorLOG("Subscription Plugin $version update: Executing SQL => $sql");
-        DB_query($sql, '1');
+        COM_errorLog("Subscription Plugin $version update: Executing SQL => $sql");
+        DB_query($sql, 1);
         if (DB_error()) {
-            COM_errorLog("SQL Error during Subscription Plugin update",1);
-            return 1;
-            break;
+            COM_errorLog("SQL Error during Subscription Plugin update", 1);
+            return false;
         }
     }
-    return 0;
+    COM_errorLog("--- Subscription plugin SQL update to version $version done", 1);
+    return true;
+}
+
+
+/**
+*   Update the plugin version number in the database.
+*   Called at each version upgrade to keep up to date with
+*   successful upgrades.
+*
+*   @param  string  $ver    New version to set
+*   @return boolean         True on success, False on failure
+*/
+function SUBSCR_do_set_version($ver)
+{
+    global $_TABLES, $_CONF_SUBSCR;
+
+    // now update the current version number.
+    $sql = "UPDATE {$_TABLES['plugins']} SET
+            pi_version = '{$_CONF_SUBSCR['pi_version']}',
+            pi_gl_version = '{$_CONF_SUBSCR['gl_version']}',
+            pi_homepage = '{$_CONF_SUBSCR['pi_url']}'
+        WHERE pi_name = '{$_CONF_SUBSCR['pi_name']}'";
+
+    $res = DB_query($sql, 1);
+    if (DB_error()) {
+        COM_errorLog("Error updating the {$_CONF_SUBSCR['pi_display_name']} Plugin version",1);
+        return false;
+    } else {
+        return true;
+    }
 }
 
 ?>
